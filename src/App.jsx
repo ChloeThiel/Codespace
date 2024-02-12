7580dbe229f126ae208110b9b3e1c6539e2ab2ac
Chloé <58678616+ChloeThiel@users.noreply.github.com>
2024-02-12 07:31:02 +0000

    codespace

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Chloe THIEL",
+  title: "Developpeuse Web, ",
+  email: "chloethiel201@gmail.com",
+  gitHub: "ChloeThiel",
+  instagram: "chloe.raconte",
+  linkedIn: "",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
